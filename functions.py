4e67708b958276880ba79fd2d11fd1367bbd5b2b
MohamedELHAFA <148880086+MohamedELHAFA@users.noreply.github.com>
2023-12-08 12:30:41 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,9 @@
 
+def produce_default_dict():
+    default_dict = {'root': 'password'}
+    return default_dict
+
+# test
+if __name__ == "__main__":
+    result = produce_default_dict()
+    print(result)
